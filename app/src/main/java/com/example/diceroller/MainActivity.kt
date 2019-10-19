acab6385b9356a70d0eccc18e1d5ab779478c589
JiaLeng99 <ngjl-wm17@student.tarc.edu.my>
2019-10-19 16:56:47 +0800

    Done.

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -15,15 +15,32 @@ class MainActivity : AppCompatActivity() {
 
         val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.setOnClickListener { rollDice() }
+
+        val countUpButton: Button = findViewById(R.id.countUp_button)
+        countUpButton.setOnClickListener{ countUp() }
     }
 
     private fun rollDice(){
         Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
 
-        val resultText: TextView = findViewById(R.id.result_text)
-        resultText.text = "Dice Rolled!"
-
         val randomInt = Random().nextInt(6) + 1
+        val resultText: TextView = findViewById(R.id.result_text)
         resultText.text = randomInt.toString()
+    }
+
+    private fun countUp(){
+        val resultText: TextView = findViewById(R.id.result_text)
+        if(resultText.text == "Hello World!"){
+            resultText.text = "1"
+        } else{
+            var resultInt = resultText.text.toString().toInt()
+            if(resultInt < 6){
+                resultInt++
+                resultText.text = resultInt.toString()
+            } else{
+                Toast.makeText(this, "Already reached maximum number of a dice", Toast.LENGTH_SHORT).show()
+            }
+        }
+
     }
 }
